5fa4fcc1e286161544eda1b02c3fbb7baebde1ba
Ekansh Rahul Jain <94004089+ekansh2403@users.noreply.github.com>
2022-08-25 22:13:20 +0530

    Create Predefined_Constant.cpp

---
+++ Predefined_Constant.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  //******************CONSTANT PRE DEFINED***********************
+  const float pi=3.14;
+  double radius,Area;
+  
+  cout<<"Enter the radius of the circle:";
+  cin>>radius;
+
+  Area=(pi * radius * radius);
+
+  cout<<"the area of the circle will be:"<<Area;
+
+  
+  return 0;
+}
+  
+   
+   
